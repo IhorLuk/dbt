7e4c2d565c11c02027116a8bb400a8a012a0183c
lukianov.igor@hotmail.com <lukianov.igor@hotmail.com>
2025-11-10 17:51:36 +0000

    remove groupping analysis

--- analyses/grouped_by_region.sql
+++
@@ -1,6 +0,0 @@
-SELECT rgn.origin_region, AVG(fct.avg_price) as avg_price
-FROM {{ref ('fct_avocado')}} fct
-JOIN {{ref ('dim_region')}} rgn
-ON fct.region_id = rgn.id
-GROUP BY rgn.origin_region
-ORDER BY avg_price DESC
